4a55765ca2b40eaabd5793094d7af4806f331ae8
Thomas Newman <tnewman1@ltu.edu>
2015-08-30 18:26:00 -0400

    Fixed line length to conform to PEP8

--- tests/test_restrequest.py
+++ tests/test_restrequest.py
@@ -11,31 +11,48 @@ class TestRestRequest(TestCase):
         self.sample_data = {'test': 'data'}
         self.sample_params = {'test': 'param'}
         self.proper_url = self.url + '/' + self.action
-        self.rest_request = RestRequest(self.url, rest_client=self.mock_requests)
+        self.rest_request = RestRequest(self.url,
+                                        rest_client=self.mock_requests)
+        self.assert_mock_requests_called_with = \
+            self.mock_requests.request.assert_called_with
 
     def test_get_sets_proper_request(self):
         self.rest_request.get(self.action, self.sample_params)
-        self.mock_requests.request.assert_called_with('GET', self.proper_url, data=None, params=self.sample_params,
-                                                      headers={'content-type': 'application/json'})
+        self.assert_mock_requests_called_with('GET', self.proper_url,
+                                              data=None,
+                                              params=self.sample_params,
+                                              headers={'content-type':
+                                                       'application/json'})
 
     def test_put_sets_proper_request(self):
         self.rest_request.put(self.action, self.sample_data)
-        self.mock_requests.request.assert_called_with('PUT', self.proper_url, data=self.sample_data, params=None,
-                                                      headers={'content-type': 'application/json'})
+        self.assert_mock_requests_called_with('PUT', self.proper_url,
+                                              data=self.sample_data,
+                                              params=None,
+                                              headers={'content-type':
+                                                       'application/json'})
 
     def test_post_sets_proper_request(self):
         self.rest_request.post(self.action, self.sample_data)
-        self.mock_requests.request.assert_called_with('POST', self.proper_url, data=self.sample_data, params=None,
-                                                      headers={'content-type': 'application/json'})
+        self.assert_mock_requests_called_with('POST', self.proper_url,
+                                              data=self.sample_data,
+                                              params=None,
+                                              headers={'content-type':
+                                                       'application/json'})
 
     def test_delete_sets_proper_request(self):
         self.rest_request.delete(self.action)
-        self.mock_requests.request.assert_called_with('DELETE', self.proper_url, data=None, params=None,
-                                                      headers={'content-type': 'application/json'})
+        self.assert_mock_requests_called_with('DELETE', self.proper_url,
+                                              data=None, params=None,
+                                              headers={'content-type':
+                                                       'application/json'})
 
     def test_auth_token_header_when_auth_token_set(self):
         self.rest_request.auth_token = 'TOKEN'
         self.rest_request.delete(self.action)
-        self.mock_requests.request.assert_called_with('DELETE', self.proper_url, data=None, params=None,
-                                                      headers={'content-type': 'application/json',
-                                                               'X-Auth-Token': 'TOKEN'})
+        self.assert_mock_requests_called_with('DELETE', self.proper_url,
+                                              data=None, params=None,
+                                              headers={'content-type':
+                                                       'application/json',
+                                                       'X-Auth-Token':
+                                                       'TOKEN'})
