50a5f01aab4a00e6a25cc28dcfb638864f4cbdea
Thomas Newman <tnewman1@ltu.edu>
2015-08-30 18:17:34 -0400

    Added RESTful request client for Kazoo

--- pykazoo/restrequest.py
+++ pykazoo/restrequest.py
@@ -2,5 +2,31 @@ import requests
 
 
 class RestRequest:
-    def __self__(self, requestapi=requests):
-        self.requestapi = requestapi
+    def __init__(self, api_url, auth_token=None, rest_client=requests):
+        self.api_url = api_url
+        self.auth_token = auth_token
+        self.rest_client = rest_client
+
+    def get(self, action, params=None):
+        return self._request('GET', action, None, params)
+
+    def put(self, action, data=None):
+        return self._request('PUT', action, data, None)
+
+    def post(self, action, data=None):
+        return self._request('POST', action, data, None)
+
+    def delete(self, action):
+        return self._request('DELETE', action, None, None)
+
+    def _request(self, verb, action, data, params):
+        self.headers = {'content-type': 'application/json'}
+
+        if self.auth_token:
+            self.headers['X-Auth-Token'] = self.auth_token
+
+        url = str(self.api_url) + '/' + str(action)
+
+        response = self.rest_client.request(verb, url, headers=self.headers, data=data, params=params)
+
+        return response
